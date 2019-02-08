f7cb3a7cfbf9b35bba7b3202170d68aec14fd429
onlylonely <ln222tu@student.lnu.se>
2019-02-08 11:53:11 +0100

    creating js files and put them together

--- hangman-game/app.js
+++ hangman-game/app.js
@@ -1,2 +1,7 @@
+const fnkModule = require('./lib/utils.js')
+fnkModule.sayHello()
 
-console.log('hello hello')
+const Game = require('./src/Game.js')
+let game = new Game()
+
+game.sayHello()
---
+++ hangman-game/src/Game.js
@@ -0,0 +1,7 @@
+class Game {
+  sayHello () {
+    console.log('hello from class module')
+  }
+}
+
+module.exports = Game
