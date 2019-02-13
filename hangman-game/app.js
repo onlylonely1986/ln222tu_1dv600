af5f941fa0684f4643917141c6f23e8d8258efb1
onlylonely <ln222tu@student.lnu.se>
2019-02-13 15:16:34 +0100

    installed a new package of inquirer and build some simple code´

--- hangman-game/app.js
+++ hangman-game/app.js
@@ -1,7 +1,32 @@
 const fnkModule = require('./lib/utils.js')
-fnkModule.sayHello()
+// fnkModule.sayHello()
 
 const Game = require('./src/Game.js')
 let game = new Game()
 
-game.sayHello()
+// game.sayHello()
+
+const inquirer = require('inquirer')
+
+let wordBank = ['elephant', 'computer', 'apple', 'lemon']
+let underScores = []
+let randWord = wordBank[Math.floor(Math.random() * wordBank.length)]
+
+for (let i = 0; i < randWord.length; i++) {
+  underScores.push('_ ')
+}
+
+console.log('Welcome to hangman game!')
+console.log(' ')
+console.log('Do you want to guess the word? Be aware that you might be hanged!')
+console.log(' ')
+var questions = [{
+  type: 'input',
+  name: 'letter',
+  message: `The word has ${underScores} letters!`
+}]
+
+inquirer.prompt(questions).then(answers => {
+  console.log(' ')
+  console.log(`Your first guess is ${answers['letter']}`)
+})
